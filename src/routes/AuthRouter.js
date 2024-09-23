7fbea44d3b840b1f78d9c07ce94d89262acf0549
HIBA BEGHDI <beghiba@gmail.com>
2024-09-24 00:08:09 +0100

    Feat : register and login using JWT, and I created a file that it.ll contain all the consts that i'll export in another file (for better structure)

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,17 +1,13 @@
 const express = require("express");
-
+const { register, login } = require("../controllers/authController");
 const router = express.Router();
 
-// router.post("/register", (req,res) => {
-//     res.json({ message : "Register the admin"});
-// });
+router.post("/register", register );
 
-// router.post("/login", (req,res) => {
-//     res.json({ message : "login admin"});
-// });
+router.post("/login", login);
 
-// router.post("/current", (req,res) => {
-//     res.json({ message : "current user information"});
-// });
+router.post("/current", (req,res) => {
+    res.json({ message : "current user information"});
+});
 
 module.exports = router;
